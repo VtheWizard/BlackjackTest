2394ad60a4a58b9b629cb72137a7a87a8135ad28
Ville Prittinen <villepri@hotmail.com>
2022-03-22 11:17:53 +0200

    dragged the .py file in after making the git repo

---
+++ BlackjackTest.py
@@ -0,0 +1,138 @@
+import random
+import os
+
+deck = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"]*4
+Dealerhand = []
+Playerhand = []
+
+def Deal(deck):
+    hand = []
+    for i in range(2):
+        random.shuffle(deck)
+        card = deck.pop()
+        hand.append(card)
+    return hand
+
+def Total(hand):
+    total = 0
+    for card in hand:
+        if card == "J":
+            total += 10
+        elif card == "Q":
+            total += 10
+        elif card == "K":
+            total += 10
+        elif card == "A":
+            if total >= 11:
+                total += 1
+            else: total += 11
+        else: total += int(card)
+    return total
+
+def checkBlackjack(hand, hand2):
+    blackjack = 0
+    if Total(hand) == 21:
+        blackjack = 1
+        if Total(hand2) == 21:
+            blackjack = 3
+    elif Total(hand2) == 21:
+        blackjack = 2
+    else: blackjack = 0
+    return blackjack
+
+def checkBust(hand):
+    if Total(hand) > 21:
+        return True
+    else: return False
+
+def hit(hand):
+    card = deck.pop()
+    hand.append(card)
+
+def resetDecks(deck):
+    deck = [1,2,3,4,5,6,7,8,9,10,"J","Q","K","A"]*4
+    return deck
+
+def playAgain():
+    again = input("Do you want to play again? (Y/N) : ")
+    if again == "y":
+        game()
+
+    else:
+        print("Bye!\n")
+        exit()
+
+def clear():
+    print("Clear!\n")
+    if os.name =='nt':
+        os.system('CLS')
+    if os.name == 'posix':
+        os.system('clear')
+
+def game():
+    choice = 0
+    clear()
+    resetDecks(deck)
+    print("Welcome to Blackjack \n")
+    Dealerhand = Deal(deck)
+    Playerhand = Deal(deck)
+    print("Your hands hand is : "+ str(Playerhand[0]) +" and "+ str(Playerhand[1]) + " which totals to " + str(Total(Playerhand)) + "\n Dealers first card is: " + str(Dealerhand[0])+"\n")
+    blackjackCheckedResult = checkBlackjack(Playerhand, Dealerhand)
+    if blackjackCheckedResult == 1:
+        print("You have a blackjack! \n")
+        playAgain()
+    elif blackjackCheckedResult == 2:
+        while checkBust(Playerhand)!=True:
+            choice = input("Choose to hit or stand\n")
+            if choice == "hit":
+                hit(Playerhand)
+                print("You got " + str(Playerhand[-1]) + "and totals to "+ str(Total(Playerhand)) + "!\n")
+                if checkBust(Playerhand) == True:
+                    print("Player Bust!\n")
+                    playAgain()
+            elif choice == "stand":
+                print("Dealers second card is: " + str(Dealerhand[1]) + " and Dealers hand totals to " + str(Total(Dealerhand)) + "!\n")
+                print("Dealer has a blackjack! You LOOSE!\n")
+                playAgain()
+        
+    elif blackjackCheckedResult == 3:
+        print("Both Player and Dealer have a blackjack! It's a TIE!\n")
+        playAgain()
+    else: 
+        print("No Blackjacks.\n")
+        while checkBust(Playerhand)!=True:
+            choice = input("Choose to hit or stand\n")
+            if choice == "hit":
+                hit(Playerhand)
+                print("You got " + str(Playerhand[-1]) + " and total to "+ str(Total(Playerhand)) + "!\n")
+                if checkBust(Playerhand) == True:
+                    print("Player Bust!\n")
+                    playAgain()
+            elif choice == "stand":
+                print("Dealers second card is: " + str(Dealerhand[1]) + " and Dealers hand totals to " + str(Total(Dealerhand)) + "!\n")
+                if Total(Dealerhand) < 17:
+                    print("Dealer totals to less than 17 and has to hit\n")
+                    hit(Dealerhand)
+                    print("Dealer got " + str(Dealerhand[-1]) + " and totals to "+ str(Total(Dealerhand)) + "!\n")
+                    if checkBust(Dealerhand) == True:
+                        print("Dealer Bust and You WIN\n")
+                        playAgain()
+                    elif Total(Dealerhand) >= 17 and checkBust(Dealerhand) == False:
+                        print("Dealer totals to 17 or more and will not hit anymore.\n")
+                elif Total(Dealerhand) >= 17 and checkBust(Dealerhand) == False:
+                    print("Dealer totals to 17 or more and will not hit anymore.\n")
+                if Total(Playerhand) <= Total(Dealerhand):
+                    print("Player totals to less than the Dealer. You loose!\n")
+                    playAgain()
+                else:
+                    print("Player totals to more than the Dealer. You Win!\n")
+                    playAgain()
+
+                      
+                
+
+if __name__ == "__main__":
+   game()
+
+
+
